96b7a29eba6b2d7ac187108d72ed7e089f599b08
AnhTuan9420 <tuanva9420@gmail.com>
2023-10-13 01:53:08 +0700

    creat chat

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -3,14 +3,12 @@ const ApiError = require('../utils/ApiError');
 const pick = require('../utils/pick');
 const catchAsync = require('../utils/catchAsync');
 const chatService = require('../services/chat.service');
-const Chat = require('../models/chat.model');
-const { matchFilter, sortAndLimit, formatData, calculateTotalResults } = require('../helper/functions');
+const { matchFilter } = require('../helper/functions');
 
 const create = catchAsync(async (req, res) => {
   const data = req.body;
   data.from = req.user.id;
   const item = await chatService.create(data);
-
   // eslint-disable-next-line no-undef
   _io.to([data.from, data.to]).emit('message', { from: req.user.id, message: data.content });
   res.status(httpStatus.CREATED).send(item);
@@ -19,54 +17,9 @@ const create = catchAsync(async (req, res) => {
 const query = catchAsync(async (req, res) => {
   const filter = matchFilter(req.user.id, req.query.to);
   const options = pick(req.query, ['sortBy', 'limit', 'page']);
-  const limit = options.limit || 6;
-  const page = options.page || 1;
 
-  const aggregationPipeline = [
-    {
-      $match: filter,
-    },
-    {
-      $sort: { createdAt: 1 },
-    },
-    {
-      $group: {
-        _id: {
-          year: { $year: '$createdAt' },
-          month: { $month: '$createdAt' },
-          day: { $dayOfMonth: '$createdAt' },
-        },
-        content: { $push: '$$ROOT' },
-      },
-    },
-    ...sortAndLimit(limit, page),
-    {
-      $project: {
-        _id: 0,
-        content: 1,
-        date: {
-          year: '$_id.year',
-          month: '$_id.month',
-          day: '$_id.day',
-        },
-      },
-    },
-    {
-      $sort: { date: -1 },
-    },
-  ];
-
-  const result = await Chat.aggregate(aggregationPipeline);
-  const totalResults = calculateTotalResults(result);
-
-  const paginatedResult = {
-    results: await formatData(result),
-    page: Number(page),
-    limit: Number(limit),
-    totalResults: calculateTotalResults(result),
-    totalPages: Math.ceil(totalResults / Number(limit)),
-  };
-  res.status(httpStatus.OK).send(paginatedResult);
+  const result = await chatService.query(filter, options);
+  res.status(httpStatus.OK).send(result);
 });
 
 const getOne = catchAsync(async (req, res) => {
